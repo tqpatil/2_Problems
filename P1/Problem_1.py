edee6d17e83589a9de17f690ba8d3b65ed1d2188
Tanishq Patil <trpatil@ucsc.edu>
2024-05-31 18:55:51 -0700

    fixing problem

--- P1/Problem_1.py
+++ P1/Problem_1.py
@@ -1,21 +1,34 @@
 import hashlib
 import time
+import sys
+import os
 unique= set()
 def dedup(inputFile, outputFile): ## inputfile: file pointer, outputfile: file pointer
-    for line in inputFile:
-        h = hashlib.new('sha256')
-        h.update(line.encode())
+    for item in inputFile:
+        h = hashlib.new('sha256') ## sha256 encoded hash (may lead to memory problems but reduces collisions)
+        h.update(item.encode())
         hash = h.hexdigest()
         if not (hash in unique):
-            outputFile.write(line)
+            outputFile.write(item)
             unique.add(hash)
     return len(unique)
 if __name__ == "__main__":
-    inputFile = open('input.txt', 'r',encoding="ascii") 
-    outputFile = open('output.txt', 'w', encoding="ascii")
+    if(len(sys.argv)< 3):
+        print("Please pass in input file path and output file path as arguments")
+        sys.exit()
+    inFilePath =sys.argv[1]
+    outFilePath = sys.argv[2] 
+    if not (os.path.exists(inFilePath)):
+        print("Error: input file doesn't exist")
+        sys.exit()
+    inputFile = open(inFilePath, 'r',encoding="ascii") 
+    outputFile = open(outFilePath, 'w', encoding="ascii")
     initial = time.time()
-    print(dedup(inputFile, outputFile))
+    print("Total unique items:", dedup(inputFile, outputFile))
     print(f"Total time taken =", time.time()-initial)
+
+
+
     # all_characters = set(chr(i) for i in range(128))
     # with open("input.txt", "w",encoding="ascii") as f:
     #     for i in range(10000):
